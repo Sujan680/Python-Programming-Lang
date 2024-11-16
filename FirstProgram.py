2f70637cf7f38bf7aaffa4d204197ee12010ab21
sujan <budhasujan3@gmail.com>
2024-11-16 18:12:24 +0545

    Python programming first commit

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,3 +1,32 @@
-
+# Python for: web development, software devlopment, mathematics, system scripting.....
 print("Hello World")
 print("Python is a popular programming language")
+
+
+print(34)
+
+print(3+5)
+
+#variable in Python:: Variables are container for storing data values.
+
+# name = 'Sujan'
+# age = 25
+# print(name)
+# print(age)
+# print(type(name))
+# print(type(age))
+
+# myName = "Sujan"
+# lastName = 'Magar'
+# print(myName, lastName);
+
+
+x = y = z = "Orange"
+print(x)
+
+
+# Casting in Python
+
+# x = str(3)
+# print(x)
+# print(type(x))
