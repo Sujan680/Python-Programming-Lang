15a4aa7c7c30e044b14c188563c900806d8d32c2
sujan <budhasujan3@gmail.com>
2024-11-17 14:02:11 +0545

    Day 2 revision test of python

--- Day2Practices.py
+++ Day2Practices.py
@@ -1,4 +1,23 @@
-num1 = int(input("Enter first number"))
-num2 = int(input("Enter second number"))
-sum = num1  + num2
-print("The sum is :", sum)
+# #To input two numbmers from the user and print sum
+# num1 = int(input("Enter first number:"))
+# num2 = int(input("Enter second number:"))
+# sum = num1  + num2
+# print("The sum is :", sum)
+
+
+# #To find area of rectangle
+# length = int(input("Enter the length:"))
+# breadth = int(input("Enter the breadth:"))
+# print("The area of square is", length*breadth)
+
+
+#To find the average 
+a = int(input("First number:"))
+b = int(input("Second number:"))
+print("Average is:", (a + b) / 2 )
+
+
+a = int(input("First number:"))
+b = int(input("Second number:"))
+
+print( a >= b)
