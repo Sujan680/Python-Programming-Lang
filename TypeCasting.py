fb4e23547d26ce233054ca750cfe7e35bd0fa794
sujan <budhasujan3@gmail.com>
2024-11-17 13:06:14 +0545

    Type Casting in python

---
+++ TypeCasting.py
@@ -0,0 +1,30 @@
+a = 34
+b = '3'
+b = int(b)
+print(a + b)
+
+
+
+# Integers
+x = int(1)
+y = int(2.4)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+# Float
+x = float(1)
+y = float(2.4)
+z = float("3")
+print(x)
+print(y)
+print(z)
+
+#Strings
+x = str(1)
+y = str(2.4)
+z = str("string")
+print(x)
+print(y)
+print(z)
