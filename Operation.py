4a00467df76dd43b9068463a288c88deff22f1e7
sujan <budhasujan3@gmail.com>
2024-11-17 12:55:12 +0545

    Operation in python

---
+++ Operation.py
@@ -0,0 +1,64 @@
+# Python Arithmetic Operators
+x =  6
+y =  5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+
+# Assignment Operators
+
+a = 5
+print(a)
+
+a += 3
+print(a)
+
+a -= 3
+print(a)
+
+a &= 3
+print(a)
+
+a ^= 3
+print(a)
+
+
+# Relaional Operators
+
+num1 = 2
+num2 = 4
+
+print(num1 == num2) #False
+print(num1 != num2) #True
+print(num1 > num2) #False
+print(num1 < num2) #True
+print(num1 <= num2) #True
+print(num1 >= num2) #False
+
+
+
+#Logical Operators
+
+#Not Operator
+print(not True)
+print(not False)
+
+# And Operator: Returns true if both statement or condition are true
+x = 10
+y = 5
+print(x > 3 and y < 10)
+
+#Or operator: Returns true if any one of the statement is true
+print( x < 4 or y > 3)
+
+
+#Bitwise & operator
+print(6 & 3) # 2
+
+print((6+3) - (7+2))
