9a24f35b687c08e01e1335911353fb6d30b3f1a4
sujan <budhasujan3@gmail.com>
2024-11-17 13:29:28 +0545

    Type Casting in python

--- TypeCasting.py
+++ TypeCasting.py
@@ -28,3 +28,17 @@ z = str("string")
 print(x)
 print(y)
 print(z)
+
+
+# input statment in Python
+number = int(input("Enter your fav number:"))
+print(type(number))
+print("Fav Number is ", number)
+
+name = input("Entere your name:")
+age = int(input("Enter your age:"))
+marks = float(input("Enter your marks:"))
+
+print("Welcome", name)
+print("Your age is:", age)
+print("Your marks is:", marks)
