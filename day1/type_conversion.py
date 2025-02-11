5db09510249c504ff2c78bda56b8dcf88c1707fe
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2025-02-11 18:32:46 +0000

    type_converison in python and type casting

---
+++ day1/type_conversion.py
@@ -0,0 +1,30 @@
+
+# type conversion (implicit conversion)
+
+a = 5;
+b = 5.5;
+sum = a + b;  # 2.0 + 5.5
+print(sum);
+
+
+# type casting
+x = "5";
+a = int(x);
+b = 3;
+print("The sum is",  (a + b));
+
+
+x = int(1);
+y = int(2.8);
+z = int("3");
+print(x);
+print(y);
+print(z);
+
+
+x = str("s1");
+y = str(2);
+z = str(3.0);
+print(x);
+print(y);
+print(z);
