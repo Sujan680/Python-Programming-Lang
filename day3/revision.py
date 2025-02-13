96d4316ad604770814f5813e7ea09f53334d8c21
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2025-02-13 13:11:17 +0000

    revision of string in python in day 3

---
+++ day3/revision.py
@@ -0,0 +1,41 @@
+
+################ Strings ###########
+
+str1  = "Hello"
+str2 = "World"
+print(str1 + " " + str2)
+
+my_str = "Hi I am sujan magar"
+print(len(my_str))
+print(type(my_str))
+
+print(my_str[0])
+print(my_str[1])
+print(my_str[5])
+
+
+# slicing 
+
+str = "Hello"
+print(str[1:3])
+print(str[:3])
+print(str[2:])
+print(str[-3:-2])
+
+########## string functions
+
+str = "I am a coder"
+print(str.endswith("I"))
+
+print(str.endswith("coder"))
+
+print(str.capitalize())
+
+print(str.lower())
+
+print(str.replace("I", "I'm"))
+print(str.find("coder"))
+
+str = "            sujan  "
+print(str.strip())
+
