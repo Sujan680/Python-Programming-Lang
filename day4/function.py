db32ddc4e9dafff4ff897e44ce8f309d219f20c0
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2025-02-14 17:18:55 +0000

    Functions in python

--- day4/function.py
+++ day4/function.py
@@ -22,3 +22,37 @@ def function(fname, lname):
     print("My name is ", fname + " " + lname)
     
 function("Suman","Sharma")
+
+
+
+# default parameter
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+
+
+def calc_avg(a,b,c):
+    s = int((a + b + c )/3)
+    return s
+print("The average is :", calc_avg(2,4,6))
+
+
+# finding the length of a list
+nums = [2,4,6,8,10,12,14,16,18,20]
+
+def calc_len(list):
+    print(len(list))
+
+calc_len(nums)
+
+
+
+def print_list(list):
+    for x in list:
+        print(x)
+      
+        nums = [1,2,3,4,5]  
+print_list(nums)
