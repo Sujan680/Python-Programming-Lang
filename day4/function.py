07b15d83e75d6224a9da3fe5e56c264a6cdfdb81
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2025-02-14 16:57:08 +0000

    Functions in python

---
+++ day4/function.py
@@ -0,0 +1,24 @@
+
+# Functions in python:
+# Block of code that perform a specific task;
+
+# syntax:::
+# def func_name(param1, param2, ......):
+#     some work
+#     return val
+
+def my_function():
+    print("Hello Python developers")
+my_function()
+
+def sum(a,b):
+    s = a + b
+    return s
+
+print("Sum is:", sum(1,2))
+
+
+def function(fname, lname):
+    print("My name is ", fname + " " + lname)
+    
+function("Suman","Sharma")
